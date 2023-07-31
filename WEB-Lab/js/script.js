569ac155d56024d6d36d11080ec37c33701dab9b
MalinduIsuru <140944275+MalinduIsuru@users.noreply.github.com>
2023-07-31 18:48:50 +0530

    Add files via upload

---
+++ WEB-Lab/js/script.js
@@ -0,0 +1,16 @@
+function validateForm(){
+    let name = document.getElementById('username').value;
+    let pass = document.getElementById('pass').checked;
+
+
+    if(name == "" && pass == "" && pass.length < 8){
+        window.alert("Plaese check atleast 1 value");
+    }
+    else{
+        window.alert("Form submitted successfully")
+    }
+}
+
+function resetForm() {
+    document.getElementById('form2').reset();
+  }
